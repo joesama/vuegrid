1d8c46a30ab9c605b37cafc712b41e627721e9fa
Johari <joharijumali@gmail.com>
2018-04-27 01:45:50 +0800

    change app - env

--- resources/config/vuegrid.php
+++ resources/config/vuegrid.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'env'   => app('APP_ENV', 'production'),
-    'debug' => app('APP_DEBUG', false),
+    'env'   => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
 ];
